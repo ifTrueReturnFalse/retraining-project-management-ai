5160119045c345035d4794afe55196837d472e5d
Timothe Duquesne <timothe.duquesne@gmail.com>
2026-03-04 11:11:26 +0100

    update: added more icons, changed named / typo

--- src/components/Menu/MenuBar/MenuBar.tsx
+++ src/components/Menu/MenuBar/MenuBar.tsx
@@ -1,14 +1,14 @@
 import styles from "./MenuBar.module.css";
 import Image from "next/image";
-import KanbanLogo from "@/components/Icons/KanbanLogo";
+import DashboardLogo from "@/components/Icons/DashboardLogo";
 import FolderIcon from "@/components/Icons/FolderIcon";
 import MenuItem from "../MenuItem/MenuItem";
 import { MenuItemProps } from "../MenuItem/MenuItem";
 import UserIcon from "@/components/Icons/UserIcon/UserIcon";
 
 const menuItems: MenuItemProps[] = [
-  { text: "Tableau de bord", Icon: KanbanLogo },
-  { text: "Kanban", Icon: FolderIcon },
+  { text: "Tableau de bord", Icon: DashboardLogo },
+  { text: "Projets", Icon: FolderIcon },
 ];
 
 export default function MenuBar() {
