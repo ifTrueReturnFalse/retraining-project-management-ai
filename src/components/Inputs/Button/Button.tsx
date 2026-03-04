f9c35abd990dda28f5480abe5cf18649e7bb883f
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-03-04 21:42:31 +0100

    update: Button accept classname

--- src/components/Inputs/Button/Button.tsx
+++ src/components/Inputs/Button/Button.tsx
@@ -4,17 +4,19 @@ interface ButtonProps {
   textButton: string;
   isSubmit?: boolean;
   disabled?: boolean;
+  className?: string;
 }
 
 export default function Button({
   textButton,
   isSubmit = false,
   disabled = false,
+  className = "",
 }: ButtonProps) {
   return (
     <>
       <button
-        className={styles.button}
+        className={`${styles.button} ${className}`}
         type={isSubmit ? "submit" : "button"}
         disabled={disabled ? true : false}
       >
