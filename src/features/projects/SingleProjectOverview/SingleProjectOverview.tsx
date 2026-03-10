d5a2f49fadd6bd199104a2084b125e7d2efc3733
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-03-10 21:31:26 +0100

    update: move the top part of the single project view in a component

---
+++ src/features/projects/SingleProjectOverview/SingleProjectOverview.tsx
@@ -0,0 +1,21 @@
+import styles from "./SingleProjectOverview.module.css";
+import Button from "@/components/Inputs/Button/Button";
+import AIButton from "@/components/AIButton/AIButton";
+
+export default function SingleProjectOverview() {
+  return (
+    <div className={styles.head}>
+      <div className={styles.projectDetail}>
+        <div className={styles.titleEdit}>
+          <h2>Nom du projet</h2>
+          <p>Modifier</p>
+        </div>
+        <p className={styles.description}>Description du projet</p>
+      </div>
+      <div className={styles.buttonsContainer}>
+        <Button textButton="Créer une tâche" />
+        <AIButton isLarge={true} />
+      </div>
+    </div>
+  );
+}
