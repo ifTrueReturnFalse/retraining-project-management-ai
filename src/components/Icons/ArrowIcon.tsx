739cb9617c60fc0523bd2e76f524ca9b51daaaa1
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-03-10 23:01:04 +0100

    update: added an arrow icon component

---
+++ src/components/Icons/ArrowIcon.tsx
@@ -0,0 +1,16 @@
+import { SVGProps } from "react";
+
+const ArrowIcon = (props?: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="17"
+    height="10"
+    viewBox="0 0 17 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M0.353516 0.353546L8.35352 8.35355L16.3535 0.353546" />
+  </svg>
+);
+
+export default ArrowIcon;
