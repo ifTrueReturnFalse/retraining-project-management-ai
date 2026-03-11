84fad47d777fcd4e6c173f1847599ac9ce93e956
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-03-11 23:05:42 +0100

    update: added a back button on single project page

--- src/features/projects/SingleProjectOverview/SingleProjectOverview.tsx
+++ src/features/projects/SingleProjectOverview/SingleProjectOverview.tsx
@@ -1,10 +1,12 @@
 import styles from "./SingleProjectOverview.module.css";
 import Button from "@/components/Inputs/Button/Button";
 import AIButton from "@/components/AIButton/AIButton";
+import IconButton from "@/components/Inputs/IconButton/IconButton";
 
 export default function SingleProjectOverview() {
   return (
     <div className={styles.head}>
+      <IconButton className={styles.backButton}>&larr;</IconButton>
       <div className={styles.projectDetail}>
         <div className={styles.titleEdit}>
           <h2>Nom du projet</h2>
