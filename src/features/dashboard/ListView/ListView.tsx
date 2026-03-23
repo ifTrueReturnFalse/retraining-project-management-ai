fa7e9ed93a0bb2cb412798e8e25660c8a386cd02
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-03-23 22:09:43 +0100

    update: now sure that tasks are displayed with the priority or with due date

--- src/features/dashboard/ListView/ListView.tsx
+++ src/features/dashboard/ListView/ListView.tsx
@@ -1,10 +1,19 @@
 import styles from "./ListView.module.css";
 import SearchBar from "../../../components/SearchBar/SearchBar";
 import ListTask from "../ListTask/ListTask";
-import { Task } from "@/models/tasks.model";
+import { Task, TaskPriorityLabels } from "@/models/tasks.model";
 
 export default function ListView({ tasks }: { tasks: Task[] }) {
   if (!tasks.length) return <p>Pas de tâche assignée.</p>;
+
+  const sortedTasks = [...tasks].sort((a, b) => {
+    const priority =
+      TaskPriorityLabels[b.priority].weight -
+      TaskPriorityLabels[a.priority].weight;
+    if (priority !== 0) return priority;
+
+    return new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime();
+  });
 
   return (
     <div className={styles.container}>
@@ -17,7 +26,7 @@ export default function ListView({ tasks }: { tasks: Task[] }) {
       </div>
 
       <div className={styles.viewListTasks}>
-        {tasks.map((task) => (
+        {sortedTasks.map((task) => (
           <ListTask key={task.id} task={task} />
         ))}
       </div>
--- src/models/tasks.model.ts
+++ src/models/tasks.model.ts
@@ -1,13 +1,27 @@
 import { z } from "zod";
-import { TaskApiResponseSchema, TaskSchema, TaskStatusEnum, TaskPriorityEnum } from "@/schemas/tasks.schema";
+import {
+  TaskApiResponseSchema,
+  TaskSchema,
+  TaskStatusEnum,
+  TaskPriorityEnum,
+} from "@/schemas/tasks.schema";
 
 export type TaskApiResponse = z.infer<typeof TaskApiResponseSchema>;
-export type Task = z.infer<typeof TaskSchema>
-export type TaskStatus = z.infer<typeof TaskStatusEnum>
-export type TaskPriority = z.infer<typeof TaskPriorityEnum>
+export type Task = z.infer<typeof TaskSchema>;
+export type TaskStatus = z.infer<typeof TaskStatusEnum>;
+export type TaskPriority = z.infer<typeof TaskPriorityEnum>;
 
 export const TaskStatusLabels: Record<TaskStatus, string> = {
   TODO: "À faire",
   IN_PROGRESS: "En cours",
-  DONE: "Terminée"
-}
+  DONE: "Terminée",
+};
+
+export const TaskPriorityLabels: Record<
+  TaskPriority,
+  { label: string; weight: number }
+> = {
+  LOW: { label: "Basse", weight: 0 },
+  MEDIUM: { label: "Moyenne", weight: 1 },
+  HIGH: { label: "Haute", weight: 2 },
+};
