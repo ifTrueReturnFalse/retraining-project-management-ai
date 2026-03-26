fbfdc4cf84210725bc14a724acef309ce2e2c8c3
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-03-26 21:03:53 +0100

    update: styling

--- src/app/(app)/account/page.tsx
+++ src/app/(app)/account/page.tsx
@@ -1,6 +1,6 @@
-import styles from './AccountPage.module.css'
-import TextInput from '@/components/Inputs/TextInput/TextInput'
-import Button from '@/components/Inputs/Button/Button'
+import styles from "./AccountPage.module.css";
+import TextInput from "@/components/Inputs/TextInput/TextInput";
+import Button from "@/components/Inputs/Button/Button";
 
 export default function AccountPage() {
   return (
@@ -8,11 +8,21 @@ export default function AccountPage() {
       <h2 className={styles.title}>Mon compte</h2>
       <p className={styles.user}>User</p>
 
-      <TextInput label='Nom' placeholder='Dupont' />
-      <TextInput label='Prénom' placeholder='Amélie' />
-      <TextInput label='Email' placeholder='a.dupont@gmail.com' type='email' />
-      <TextInput label='Mot de passe' placeholder='&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;' type='password' />
-      <Button textButton='Modifier les informations' />
+      <div className={styles.inputContainer}>
+        <TextInput label="Nom" placeholder="Dupont" />
+        <TextInput label="Prénom" placeholder="Amélie" />
+        <TextInput
+          label="Email"
+          placeholder="a.dupont@gmail.com"
+          type="email"
+        />
+        <TextInput
+          label="Mot de passe"
+          placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
+          type="password"
+        />
+        <Button textButton="Modifier les informations" />
+      </div>
     </section>
-  )
+  );
 }
