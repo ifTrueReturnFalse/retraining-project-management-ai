6cbf0554e448178be63e4dca3715c7c9b8b2d813
Timothe Duquesne <timothe.duquesne@gmail.com>
2026-04-02 15:46:28 +0200

    update: added modal manager in projects page

--- src/app/(app)/project/page.tsx
+++ src/app/(app)/project/page.tsx
@@ -4,12 +4,15 @@ import styles from "./ProjectPage.module.css";
 import DashProjHead from "@/components/DashProjHead/DashProjHead";
 import ProjectCard from "@/features/projects/ProjectCard/ProjectCard";
 import { useAllProjects } from "@/hooks/useProjects";
+import ModalManager from "@/components/Modals/ModalManager/ModalManager";
 
 export default function ProjectPage() {
   const {projects} = useAllProjects()
   
   return (
     <>
+      <ModalManager />
+      
       <DashProjHead title="Mes projets" description="Gérez vos projets" />
       
       <section className={styles.projectsGrid}>
