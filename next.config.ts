7c3e582942be6fd31602c7d0e29a4fb37c12f616
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-04-09 22:45:05 +0200

    update: changed config, using llamaindex on server only

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["llamaindex", "@llamaindex/mistral"]
 };
 
 export default nextConfig;
